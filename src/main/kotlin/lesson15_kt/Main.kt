77b852ef24a9432cc9c965f8d2c490975001a6ed
user474230 <user474230@yandex.ru>
2024-11-02 17:40:06 +0300

    lesson 15 Kotlin

--- src/main/kotlin/lesson15_kt/Main.kt
+++ src/main/kotlin/lesson15_kt/Main.kt
@@ -2,17 +2,14 @@ package lesson15_kt
 
 import java.nio.ByteBuffer
 import java.io.File
-import java.io.IOException
 import java.io.RandomAccessFile
 import java.nio.file.FileVisitResult
-import java.nio.file.FileVisitor
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
 import java.nio.file.SimpleFileVisitor
 import java.nio.file.attribute.BasicFileAttributes
 import kotlin.io.path.fileSize
-import kotlin.io.path.fileVisitor
 
 fun main() {
     case1("qwerty");
